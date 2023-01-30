a9e79a31037986d7e092bdbb270db598c8bdcafc
Lucas Muginoski <lucasmmuginoski@gmail.com>
2023-01-30 10:43:38 -0300

    test:feature login finalizada

--- login_test.js
+++ login_test.js
@@ -16,6 +16,35 @@ Scenario('Login com sucesso',  ({ I }) => {
     I.waitForElement('section .dashboard_tab_button');
 });
 
-//Tentando logar apenas com email
-//Tentando logar sem enviar email e senha
-//Tentando logar apenas com senha;
+Scenario('Tentando logar apenas com email',  ({ I }) => {
+
+    I.amOnPage('http://www.automationpratice.com.br/');
+    I.click('.right_list_fix a[href="/login"]');
+    I.waitForText('Login', '10');
+    I.fillField('input#user','lucas_test@email.com');
+    I.click('.login_submit button[type="submit"]');
+
+    I.waitForText('Senha inválida.', '10');
+});
+
+Scenario('Tentando logar sem enviar email e senha',  ({ I }) => {
+
+    I.amOnPage('http://www.automationpratice.com.br/');
+    I.click('.right_list_fix a[href="/login"]');
+    I.waitForText('Login', '10');
+    I.click('.login_submit button[type="submit"]');
+    I.waitForText('E-mail inválido.','10');
+    //I.waitForText('Senha inválida.', '10');
+});
+
+Scenario('Tentando logar apenas com senha',  ({ I }) => {
+
+    I.amOnPage('http://www.automationpratice.com.br/');
+    I.click('.right_list_fix a[href="/login"]');
+    I.waitForText('Login', '10');
+    I.click('input#password');
+    I.fillField('input#password', 'password123!');
+    I.click('.login_submit button[type="submit"]');
+    I.click('.login_submit button[type="submit"]');
+    I.waitForText('E-mail inválido.','10');
+});
